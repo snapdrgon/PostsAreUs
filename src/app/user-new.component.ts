26c934fb7a8d53ff0a9904d24afd83de853434d2
Lynn Kirste <estrumpswan@gmail.com>
2017-04-08 09:13:39 -0600

    added validation for user and email

--- src/app/user-new.component.ts
+++ src/app/user-new.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
-import {FormGroup, FormBuilder, FormControl} from '@angular/forms';
+import {FormGroup, FormBuilder, FormControl,Validators} from '@angular/forms';
+import {EmailValidators} from './emailvalidators';
 
 @Component({
     selector: 'user-new',
@@ -9,11 +10,11 @@ import {FormGroup, FormBuilder, FormControl} from '@angular/forms';
 
 export class UserNewComponent {
     userAddForm:FormGroup;
-
+    minLength = 5;
     constructor(fb:FormBuilder) {
         this.userAddForm = fb.group({
-            userName:[],
-            email:[],
+            userName:['' , Validators.compose([Validators.required, Validators.minLength(this.minLength)])],
+            email:['', EmailValidators.emailInvalid],
             phone:[],
             street:[],
             suite:[],
